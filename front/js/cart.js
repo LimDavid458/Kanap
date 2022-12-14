c5d6d8165ef32ad613dc92a96eaff09923b8e68b
Lim David <david.lim.0101@gmail.com>
2022-12-14 15:30:38 +0100

    optimization code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,92 +1,108 @@
-import {getFromLocalStorage,localStorageHasKey,saveToLocalStorage} from "./storage.js"
+import { getFromLocalStorage,saveToLocalStorage } from "./storage.js"
 
 const cartItems = document.getElementById("cart__items");
 const totalQuantity = document.getElementById("totalQuantity");
 const totalPrice = document.getElementById("totalPrice");
-const cartDescription = document.getElementsByClassName("cart__item__content__description");
 const cart = getFromLocalStorage();
-let product;
+let products = [];
 
-(function init() {
-    for(let object of cart) {
-        createArticles(object);
-    }
+(async function init() {
+    await createArticles();
 
-    document.addEventListener("change", function(event) {
-        if(event.target.name === "itemQuantity"){
-            const itemId = event.target.closest("article").dataset.id;
-            const itemColor = event.target.closest("article").dataset.color;
-            let quantity = Number(event.target.value);    
+    getTotalProducts();
+    getTotalPrice();
 
-            event.target.value = changeQuantity(itemId,itemColor,quantity);
-            changePrice(itemId,itemColor,quantity);
-            getNumberProduct();
+    const inputsQuantity = document.querySelectorAll('.itemQuantity');
+    const inputsDeleteItem = document.querySelectorAll('.deleteItem');
+
+    inputsQuantity.forEach(input => {
+        input.addEventListener("change", function() {
+            const article = input.closest("article");
+            const itemId = article.dataset.id;
+            const itemColor = article.dataset.color;
+            let quantity = Number(input.value);
+
+            input.value = changeQuantity(itemId,itemColor,quantity);
+
+            getTotalProducts();
             getTotalPrice();
-        }
+        });
     });
 
-    document.addEventListener("click", function(event) {
-       if(event.target.className === "deleteItem") {
-            const itemId = event.target.closest("article").dataset.id;
-            const itemColor = event.target.closest("article").dataset.color;
-            
-            removeItem(itemId,itemColor);
-       }
-    });
+    inputsDeleteItem.forEach(input => {
+        input.addEventListener("click", function() {
+            const article = input.closest("article");
 
+            removeItem(article);
+
+            getTotalProducts();
+            getTotalPrice();
+        });
+    });
 })();
 
-async function createArticles(object) { 
-    product =  await fetch(`http://localhost:3000/api/products/${object.id}`)
-    .then((response) => response.json())
-    .then((data) => { return data; });
+async function createArticles() {
+  
+    await getObjectFromLocalStorage();
+
+    cart.forEach((object, i) =>  {
+        const cardProduct = `
+            <article class="cart__item" data-id="${object.id}" data-color="${object.color}">
+                <div class="cart__item__img">
+                    <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+                </div>
+                <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>${products[i].name}</h2>
+                        <p>${object.color}</p>
+                        <p>${products[i].price * object.quantity}€</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                            <p>Qté : </p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${object.quantity}">
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        `;
+
+        cartItems.insertAdjacentHTML("beforeend",cardProduct);
+    });
+}
+
+async function getObjectFromLocalStorage() {
+    // On crée une variable contenant tous les ids stockés dans le localStorage
+    const ids = cart.map(x => x.id);
     
-    const cardProduct = `
-        <article class="cart__item" data-id="${product._id}" data-color="${object.color}">
-            <div class="cart__item__img">
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${object.color}</p>
-                    <p>${product.price*object.quantity}€</p>
-                </div>
-                <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                        <p>Qté : </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${object.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
-            </div>
-        </article> 
-    `;
+    // Ici on requête l'API plusieurs fois afin de récupérer plusieurs données
+    // La promesse renvoie les valeurs correspondantes aux requêtes qui ont été envoyées et qui récupèrent les données
+    // en fonction de ce qui est stocké dans le localStorage
+    products = await Promise.all(
+        ids.map(id => fetch(`http://localhost:3000/api/products/${id}`).then(response => response.json()))
+    );
+}
 
-    cartItems.insertAdjacentHTML("beforeend",cardProduct);
-    getNumberProduct();
-    getTotalPrice();
-};
-
-function getNumberProduct() {
+function getTotalProducts() {
     let number = 0;
+
     for(let product of cart){
         number += product.quantity;
     }
+
     totalQuantity.innerHTML = `${number}`;
 }
 
-async function getTotalPrice() {
+function getTotalPrice() {
     let price = 0;
-    for (let item of cart) {
-        product =  await fetch(`http://localhost:3000/api/products/${item.id}`)
-        .then((response) => response.json())
-        .then((data) => { return data; });
 
-        price += item.quantity*product.price;  
-    }
+    cart.forEach((object, index) => {
+        price += object.quantity * products[index].price;
+    });
+
     totalPrice.innerHTML = `${price}`;
 }
 
@@ -112,33 +128,19 @@ function changeQuantity(id,color,quantity) {
     }
 }
 
-async function changePrice(id,color,quantity) {
-   let index = 0, newPrice;
+function removeItem(article) {
+    const itemId = article.dataset.id;
+    const itemColor = article.dataset.color;
 
-    for(let item of cart) {
-        product =  await fetch(`http://localhost:3000/api/products/${item.id}`)
-        .then((response) => response.json())
-        .then((data) => { return data; });
+    const index = cart.findIndex(item => item.id === itemId && item.color === itemColor);
+    
+    // On synchronise les tableaux afin d'éviter les erreurs
+    cart.splice(index, 1);
+    products.splice(index, 1);
 
-        if (item.id === id && item.color === color ) {
-            newPrice = product.price * quantity;
-            const newDescription = `
-                <h2>${product.name}</h2>
-                <p>${color}</p>
-                <p>${newPrice}€</p>
-            `;
-            cartDescription[index].innerHTML = newDescription;
-        }
-        index++;
-    }
-}
+    article.remove();
 
-function removeItem(id,color) {
-    let foundObject = cart.find(item => item.id === id && item.color === color);
-    let newCart = cart.filter(item => item !== foundObject);
-
-    saveToLocalStorage(newCart);
-    location.reload();
+    saveToLocalStorage(cart);
 }
 
 
